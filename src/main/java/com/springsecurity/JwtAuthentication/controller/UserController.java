e97b483c58b2413d1886774634f583fc7252354b
Manoj Shrestha <shresthamanoj522@gmail.com>
2026-07-05 14:41:14 +0900

    Work on simple usercontroller

--- src/main/java/com/springsecurity/JwtAuthentication/controller/UserController.java
+++ src/main/java/com/springsecurity/JwtAuthentication/controller/UserController.java
@@ -1,4 +1,41 @@
 package com.springsecurity.JwtAuthentication.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class UserController {
+    private UserInfoService userInfoService;
+    private JwtService jwtService;
+    private AuthenticationManager authenticationManager;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome this endpoint is not secure";
+    }
+
+    @PostMapping("/addNewUser")
+    public String addNewUser(@RequestBody UserInfo userInfo) {
+        return userInfoService.addUser(userInfo);
+    }
+
+    @PostMapping("generateToken")
+    public String authenticateAndGenerateToken(@RequestBody AuthRequest authRequest) {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(),
+                            authRequest.getPassword())
+            );
+            if (authentication.isAuthenticated()) {
+                return jwtService.generateToken(authRequest.getUserName());
+            } else {
+                throw new UsernameNotFoundException("Invalid user Request!");
+            }
+    }
+
 }
