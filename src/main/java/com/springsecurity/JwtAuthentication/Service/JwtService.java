0b5e94bc5c4fa90e4945ad9eba7ff90ec38e1d85
Manoj Shrestha <shresthamanoj522@gmail.com>
2026-07-10 21:19:35 +0900

    feat:Created JwtService

--- src/main/java/com/springsecurity/JwtAuthentication/Service/JwtService.java
+++ src/main/java/com/springsecurity/JwtAuthentication/Service/JwtService.java
@@ -1,2 +1,25 @@
-package com.springsecurity.JwtAuthentication.Service;public class JwtService {
+package com.springsecurity.JwtAuthentication.Service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtService {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    // Generate Token using email
+    public String generateToken(String email) {
+        Map<Object, String> claims = new HashMap<>();
+        return createToken(claims, email);
+    }
+
+    private String createToken(Map<String, Object> claims, String email) {
+        return Jwts.
+
+    }
+
+
 }
