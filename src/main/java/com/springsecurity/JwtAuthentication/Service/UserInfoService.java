4751bb6b573281459d8d1c81c68949f6961136aa
Manoj Shrestha <shresthamanoj522@gmail.com>
2026-07-15 11:51:29 +0900

    feat: Create UserInfoservice with implements userdetailsservice

--- src/main/java/com/springsecurity/JwtAuthentication/Service/UserInfoService.java
+++ src/main/java/com/springsecurity/JwtAuthentication/Service/UserInfoService.java
@@ -1,2 +1,44 @@
-package com.springsecurity.JwtAuthentication.Service;public class UserInfoService {
+package com.springsecurity.JwtAuthentication.Service;
+
+import com.springsecurity.JwtAuthentication.Entity.UserInfo;
+import com.springsecurity.JwtAuthentication.Repository.UserInfoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserInfoService implements UserDetailsService {
+    private final UserInfoRepo userInfoRepo;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserInfoService(UserInfoRepo userInfoRepo, PasswordEncoder passwordEncoder) {
+        this.userInfoRepo = userInfoRepo;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        // fetch the username from db
+        Optional<UserInfo> userInfo = userInfoRepo.findByEmail(username);
+
+        if (userInfo.isEmpty()) {
+            throw new UsernameNotFoundException("User not found with email:" + username);
+        }
+
+        UserInfo user = userInfo.get();
+        return new UserInfoDetails(user);
+    }
+
+    public String addUser(UserInfo userInfo) {
+        // Encrypt the password
+        userInfo.setPassword(passwordEncoder.encode(userInfo.getPassword()));
+        userInfoRepo.save(userInfo);
+        return "User saved successfully";
+    }
 }
