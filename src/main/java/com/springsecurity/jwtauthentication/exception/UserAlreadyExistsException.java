c4fa7664eb362fc9b154f7d17b573091b35c4c24
Manoj Shrestha <shresthamanoj522@gmail.com>
2026-09-01 20:10:58 +0900

    feat: add UserAlreadyExistsException extending RuntimeException

--- src/main/java/com/springsecurity/jwtauthentication/exception/UserAlreadyExistsException.java
+++ src/main/java/com/springsecurity/jwtauthentication/exception/UserAlreadyExistsException.java
@@ -1,2 +1,10 @@
-package com.springsecurity.jwtauthentication.exception;public class UserAlreadyExistsException {
+package com.springsecurity.jwtauthentication.exception;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+    public UserAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
